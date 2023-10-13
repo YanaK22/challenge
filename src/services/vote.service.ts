86d59693baa695a961830a65855f7f823239d9fb
Yana Karcheuskaya <y.karcheuskaya@sam-solutions.com>
2023-10-13 11:14:57 +0300

    emulate voting

--- src/services/vote.service.ts
+++ src/services/vote.service.ts
@@ -16,7 +16,8 @@ export class VoteService implements OnDestroy  {
   //random
   randomCreateProsSec: number[] = [];
   randomCreateConsSec: number[] = [];
-  randomVoteSec: number[] = [];
+
+  isVoteProsAdv;
 
   constructor(private logService: LogService) {
     // init 5 pros & cons using mock
@@ -26,10 +27,12 @@ export class VoteService implements OnDestroy  {
     }
 
     this.generateRandomCreateItemSec();
-    this.generateRandomVoteSec();
+    this.isVoteProsAdv = !!this.getRandomNumber(0, 1);
+    console.log(`Pros has adv: ${this.isVoteProsAdv}`);
+
     // START EMULATION
     this.emitRandomCreateItemEvents();
-    // this.emitRandomVoteEvents();
+    this.emitRandomVoteEvents();
 
     this.subscriptions.add(
       this.logService.voteFinished.subscribe(() => {
@@ -76,8 +79,7 @@ export class VoteService implements OnDestroy  {
   }
 
   getRandomNumber(min: number, max: number) {
-    // 5min = 300s, generate random second
-    return +(Math.random() * (max - min) + min).toFixed();
+    return Math.floor(Math.random() * (max - min + 1) + min);
   }
 
   generateRandomCreateItemSec() {
@@ -109,15 +111,20 @@ export class VoteService implements OnDestroy  {
         }
       });
 
-      console.log(timeSec);
+      // console.log(timeSec);
     }, 1000);
   }
 
-  generateRandomVoteSec() {
-    // ATTENTION:  Sorry for this simplification, if I had more I time, I would do this better * crying *
-    for (let i = 0; i < 100; i++) {
-      this.randomVoteSec.push(this.getRandomNumber(1, 300));
-    }
+  uniq(arr: number[]) {
+    const result: any = {};
+    arr.forEach(function(a){
+      if (result[a] != undefined)
+        ++result[a];
+      else
+        result[a] = 1;
+    });
+
+    return result;
   }
 
   emitRandomVoteEvents() {
@@ -127,23 +134,49 @@ export class VoteService implements OnDestroy  {
         clearInterval(interval);
       }
 
-      const timeSec = +(this.logService.getTime() / 1000).toFixed();
+      const voteCount = this.getRandomNumber(20, 60);
+      const randomProsIndexes: number[] = [];
+      const randomConsIndexes: number[] = [];
+      for (let i = 0; i < voteCount; i++) {
+        const type = this.getRandomNumber(0, 1) ? ItemType.Pros : ItemType.Cons;
 
-      this.randomVoteSec.forEach(s => {
-        if (s === timeSec) {
-           const type = this.getRandomNumber(1, 10) < 6 ? ItemType.Pros : ItemType.Cons;
-           const itemIndex = this.getRandomNumber(0, this[type].length - 1);
-           const item = this[type][itemIndex];
-           const count = this.getRandomNumber(1, 100);
-           const isLike = this.getRandomNumber(1, 10) < 6;
-           if (isLike) {
-             this.like(item.id, type, count);
-           } else {
-             this.dislike(item.id, type, count);
-           }
+        if (type === ItemType.Pros) {
+          const randomIndex = this.getRandomNumber(0, this.pros.length - 1);
+          randomProsIndexes.push(randomIndex);
+        } else {
+          const randomIndex = this.getRandomNumber(0, this.cons.length - 1);
+          randomConsIndexes.push(randomIndex);
         }
-      });
+      }
 
+      // for more difference, but not needed
+      if (this.isVoteProsAdv) {
+        randomConsIndexes.splice(0, 5);
+      } else {
+        randomProsIndexes.splice(0, 5);
+      }
+
+      const randomProsIndexesUniq = this.uniq(randomProsIndexes);
+      for (const key in randomProsIndexesUniq) {
+        const index = +key;
+        const isLike = this.getRandomNumber(0, 1);
+        if (isLike) {
+          this.like(this.pros[index].id, ItemType.Pros, randomProsIndexesUniq[key]);
+        } else {
+          this.dislike(this.pros[index].id, ItemType.Pros, randomProsIndexesUniq[key]);
+        }
+      }
+
+      const randomConsIndexesUniq = this.uniq(randomConsIndexes);
+      for (const key in randomConsIndexesUniq) {
+        const index = +key;
+        const isLike = this.getRandomNumber(0, 1);
+        if (isLike) {
+          this.like(this.cons[index].id, ItemType.Cons, randomConsIndexesUniq[key]);
+        } else {
+          this.dislike(this.cons[index].id, ItemType.Cons, randomConsIndexesUniq[key]);
+        }
+      }
     }, 1000);
   }
 
